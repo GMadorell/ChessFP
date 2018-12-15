4eae01df2ff25eb862facec3b60122197f3475b6
GMadorell <gmadorell@gmail.com>
2018-12-15 17:27:07 +0000

    CreateChessGameUseCase domain part

--- src/main/scala/com/gmadorell/chessfp/module/chess_game/application/create/CreateChessGameUseCase.scala
+++ src/main/scala/com/gmadorell/chessfp/module/chess_game/application/create/CreateChessGameUseCase.scala
@@ -1,19 +1,31 @@
 package com.gmadorell.chessfp.module.chess_game.application.create
 
-import cats.implicits._
+import cats.syntax.functor._
+import cats.syntax.flatMap._
 import cats.MonadError
-import com.gmadorell.chessfp.module.chess_game.domain.model.ChessGameId
+import com.gmadorell.chessfp.module.chess_game.domain.model.{
+  ChessGame,
+  ChessGameId
+}
 import com.gmadorell.chessfp.module.chess_game.AppError
-import com.gmadorell.chessfp.module.chess_game.AppErrorTypeClasses.MonadErrorForAppError
 
 object CreateChessGameUseCase {
-  def createChessGame[P[_]: MonadError[?[_], AppError]](
+  def createChessGame[P[_]: MonadError[?[_], AppError]: ChessGameAlgebra](
     rawChessGameId: String
   ): P[Unit] = {
     for {
-      chessGameId <- ChessGameId.validate(rawChessGameId)(
-        MonadErrorForAppError[P]
-      )
+      chessGameId <- ChessGameId.validate(rawChessGameId)
+      chessGame = ChessGame(chessGameId)
+      _ <- ChessGameAlgebra[P].create(chessGame)
     } yield ()
   }
+}
+
+trait ChessGameAlgebra[P[_]] {
+  def create(chessGame: ChessGame): P[Unit]
+}
+
+object ChessGameAlgebra {
+  def apply[P[_]: ChessGameAlgebra]: ChessGameAlgebra[P] =
+    implicitly[ChessGameAlgebra[P]]
 }
---
+++ src/main/scala/com/gmadorell/chessfp/module/chess_game/domain/model/ChessGame.scala
@@ -0,0 +1,4 @@
+package com.gmadorell.chessfp.module.chess_game.domain.model
+import com.gmadorell.chessfp.module.chess_game.domain.model.ChessGameId.ChessGameId
+
+final case class ChessGame(id: ChessGameId)
